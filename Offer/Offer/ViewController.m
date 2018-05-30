30eaf5c5d32086ddab8adcbf6f9c5f25d224a8ba
“za-zhengyuanqian” <zhengyuanqian@zhongan.io>
2018-05-30 17:43:52 +0800

    kwkw

--- Offer/Offer/ViewController.m
+++ Offer/Offer/ViewController.m
@@ -25,6 +25,79 @@ struct ListNode {
 
 @end
 
+@interface BinaryTreeHaveParentNode : NSObject
+
+@property (nonatomic, assign) int mKey;
+@property (nonatomic, strong) BinaryTreeHaveParentNode *mLeft;
+@property (nonatomic, strong) BinaryTreeHaveParentNode *mRight;
+@property (nonatomic, strong) BinaryTreeHaveParentNode *mParent;
+
+@end
+
+@implementation BinaryTreeHaveParentNode
+
+@end
+
+@interface Queue : NSObject
+{
+    int inArr[10];
+    int inLength;
+    int outArr[10];
+    int outLength;
+}
+
+- (void)push:(int)value;
+
+- (void)pop;
+
+@end
+
+@implementation Queue
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self reset];
+    }
+    return self;
+}
+
+- (void)reset {
+    int total = sizeof(inArr)/sizeof(int);
+    memset(inArr, '\0', total);
+    inLength = 0;
+    memset(outArr, '\0', total);
+    outLength = 0;
+}
+
+- (void)push:(int)value {
+    int total = sizeof(inArr)/sizeof(int);
+    int num = 0;
+    for (; num < total - 1; ++num) {
+        if (inArr[num] == '\0') {
+            inArr[num] = value;
+            inArr[num+1] = '\0';
+            inLength++;
+            break;
+        }
+    }
+}
+
+- (void)pop {
+    if (outArr[0] == '\0') {
+        for (int num = inLength-1, time = 0; num >= 0; --num, ++time) {
+            outArr[time] = inArr[num];
+            inArr[num] = '\0';
+        }
+        outLength = inLength;
+        inLength = 0;
+    }
+    outLength--;
+    outArr[outLength-1] = '\0';
+}
+
+@end
+
 @interface ViewController ()
 
 @end
@@ -49,7 +122,22 @@ struct ListNode {
 
 //    [self createLinkWithPreAndMidOrder];
 
-    [self Construct];
+//    [self Construct];
+    
+//    [self testQueue];
+
+//    int result = [self fabonacci:3];
+//    NSLog(@"==result==%d", result);
+//    result = [self fabonacciOptimization:3];
+//    NSLog(@"==result==%d", result);
+
+//    [self SortAges];
+
+//    int min = [self findMinRotateArray];
+//    NSLog(@"====min====%d", min);
+
+    BOOL hasPath = [self hasPath];
+    NSLog(@"====hasPath====%d", hasPath);
 }
 
 //剑指 Offer 第二版 面试题3
@@ -208,32 +296,7 @@ struct ListNode {
     NSLog(@"====value====%d", header->mKey);
 }
 
-//剑指 Offer 第二版 面试题7 （有问题）
-- (void)createLinkWithPreAndMidOrder {
-    int preOrder[] = {1, 2, 4, 7, 3, 5, 6, 8};
-    int midOrder[] = {4, 7, 2, 1, 5, 3, 8, 6};
-    [self createLinkWithPreAndMidOrderCore:preOrder preStart:0 preEnd:sizeof(preOrder)/sizeof(int) midOrder:midOrder midStart:0 midEnd:sizeof(midOrder)/sizeof(int) position:0];
-}
-
-- (void)createLinkWithPreAndMidOrderCore:(int *)preOrder preStart:(int)preStart preEnd:(int)preEnd midOrder:(int *)midOrder midStart:(int)midStart midEnd:(int)midEnd position:(int)position {
-    int currentRootMidIndex = midStart;
-    for (; currentRootMidIndex <= midEnd; ++currentRootMidIndex) {
-        if (preOrder[preStart] == midOrder[currentRootMidIndex]) {
-            NSLog(@"-----node-----%d--%d", midOrder[currentRootMidIndex], position);
-            break;
-        }
-    }
-    int leftLength = currentRootMidIndex - preStart;
-    int leftPreorderEnd = preStart + leftLength;
-    if (leftLength >= 0) {
-        [self createLinkWithPreAndMidOrderCore:preOrder preStart:preStart+1 preEnd:leftPreorderEnd midOrder:midOrder midStart:midStart midEnd:currentRootMidIndex-1 position:1];
-    }
-    if (leftLength < preEnd - preStart) {
-        [self createLinkWithPreAndMidOrderCore:preOrder preStart:leftPreorderEnd+1 preEnd:preEnd midOrder:midOrder midStart:currentRootMidIndex+1 midEnd:midEnd  position:2];
-    }
-}
-
-//剑指 Offer 第二版 面试题7 （原版）
+//剑指 Offer 第二版 面试题7
 - (BinaryTreeNode *)Construct {
     int preOrder[] = {1, 2, 4, 7, 3, 5, 6, 8};
     int midOrder[] = {4, 7, 2, 1, 5, 3, 8, 6};
@@ -262,6 +325,208 @@ struct ListNode {
         node.mRight = [self ConstructCore:preOrder preStart:preStart+leftLength+1 preEnd:preEnd midOrder:midOrder midStart:currentRootMidIndex+1 midEnd:midEnd];
     }
     return node;
+}
+
+//剑指 Offer 第二版 面试题8
+- (BinaryTreeHaveParentNode *)getNextNode:(BinaryTreeHaveParentNode *)pNode {
+    if (pNode == nil) {
+        return nil;
+    }
+    if (pNode.mRight == nil) {
+        BinaryTreeHaveParentNode *parentNode = pNode.mParent;
+        if (parentNode.mLeft == pNode) {
+            return pNode.mParent;
+        }
+        else {
+            while (parentNode != nil) {
+                if (parentNode == parentNode.mParent.mLeft) {
+                    return parentNode;
+                }
+                parentNode = parentNode.mParent;
+            }
+            return pNode.mParent;
+        }
+    }
+    else {
+        BinaryTreeHaveParentNode *tempNode = pNode.mRight;
+        while (tempNode.mLeft != nil || tempNode.mRight != nil) {
+            BinaryTreeHaveParentNode *left = tempNode.mLeft;
+            if (left) {
+                return left;
+            }
+            else {
+                tempNode = tempNode.mRight;
+            }
+        }
+        return pNode.mRight;
+    }
+}
+
+//剑指 Offer 第二版 面试题9
+- (void)testQueue {
+    Queue *queue = [[Queue alloc] init];
+    [queue push:1];
+    [queue push:2];
+    [queue push:3];
+    [queue push:4];
+    [queue push:5];
+    [queue pop];
+    [queue push:6];
+    [queue push:7];
+    [queue push:8];
+    [queue push:9];
+    [queue pop];
+    NSLog(@"----");
+}
+
+//剑指 Offer 第二版 面试题10
+- (int)fabonacci:(int)n {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return [self fabonacci:n-1] + [self fabonacci:n-2];
+    }
+}
+
+//剑指 Offer 第二版 面试题10 优化
+- (int)fabonacciOptimization:(int)n {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    int one = 0;
+    int two = 1;
+    int num = 2;
+    int temp = 0;
+    while (num <= n) {
+        temp = one + two;
+        one = two;
+        two = temp;
+        num++;
+    }
+    return temp;
+}
+
+//剑指 Offer 第二版 查找（时间复杂度为 O(n)，用空间换时间）
+- (void)SortAges {
+    int ages[10000];
+    int length = sizeof(ages)/sizeof(int);
+    for (int num = 0; num < length-2; ++num) {
+        ages[num]  = arc4random() % 99;
+    }
+    
+    const int timesOfAgesLength = 99;
+    int timesOfAges[timesOfAgesLength + 1];
+    for (int num = 0; num < timesOfAgesLength; ++num) {
+        timesOfAges[num] = 0;
+    }
+    
+    for (int num = 0; num < length-1; ++num) {
+        ++timesOfAges[ages[num]];
+    }
+    
+    int index = 0;
+    for (int num = 0; num < timesOfAgesLength; ++num) {
+        for (int n = 0; n < timesOfAges[num]; ++n) {
+            ages[index++] = num;
+            NSLog(@"----current----%d, %d", num, n);
+        }
+    }
+    NSLog(@"----");
+}
+
+//剑指 Offer 第二版 面试题11
+- (int)findMinRotateArray {
+    int rotateArr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3};
+//    int rotateArr[] = {2, 0, 2, 2,2, 2,2, 2,2, 2};
+    int start = 0;
+    int end = sizeof(rotateArr)/sizeof(int) - 1;
+    while (end != start + 1) {
+        int mid = (start + end)/2;
+
+        if ((rotateArr[mid] == rotateArr[start]) && (rotateArr[start] == rotateArr[end])) {
+            return [self minInOrder:rotateArr start:start end:end];
+        }
+
+        if (rotateArr[mid] <= rotateArr[end]) {
+            end = mid;
+            continue;
+        }
+        if (rotateArr[mid] >= rotateArr[start]) {
+            start = mid;
+            continue;
+        }
+    }
+    return rotateArr[end];
+}
+
+- (int)minInOrder:(int *)arr start:(int)start end:(int)end {
+    int result = arr[start];
+    for (int num = start + 1; num < end + 1; ++num) {
+        if (result > arr[num]) {
+            result = arr[num];
+        }
+    }
+    return result;
+}
+
+//剑指 Offer 第二版 面试题12
+- (BOOL)hasPath {
+    char *matrix = "abtgcfcsjdeh";
+    BOOL visited[] = {NO, NO, NO, NO, NO, NO, NO, NO,NO, NO, NO, NO};
+//    char *str = "bfce";
+    char *str = "abfb";
+
+    int strLength = 0;
+    int rows = 3;
+    int columns = 4;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if ([self hasPathCore:matrix rows:rows cols:columns curRow:i curCol:j str:str strLength:strLength visited:visited]) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+
+- (BOOL)hasPathCore:(char *)matrix rows:(int)rows cols:(int)cols curRow:(int)curRow curCol:(int)curCol str:(char*)str strLength:(int)strLength visited:(BOOL*)visited {
+    
+    if (str[strLength] == '\0') {
+        return YES;
+    }
+    
+    int curIndex = curRow * cols + curCol;
+    BOOL hasPath = NO;
+    if (str[strLength] == matrix[curIndex] &&
+        !visited[curIndex] &&
+        curRow >= 0 &&
+        curCol >= 0 &&
+        curRow < rows &&
+        curCol < cols) {
+        
+        strLength++;
+        visited[curIndex] = YES;
+        
+        BOOL leftHasPath = [self hasPathCore:matrix rows:rows cols:cols curRow:curRow curCol:curCol-1 str:str strLength:strLength visited:visited];
+        BOOL rightHasPath = [self hasPathCore:matrix rows:rows cols:cols curRow:curRow curCol:curCol+1 str:str strLength:strLength visited:visited];
+        BOOL topHasPath = [self hasPathCore:matrix rows:rows cols:cols curRow:curRow-1 curCol:curCol str:str strLength:strLength visited:visited];
+        BOOL bottomHasPath = [self hasPathCore:matrix rows:rows cols:cols curRow:curRow+1 curCol:curCol str:str strLength:strLength visited:visited];
+        
+        hasPath = leftHasPath || rightHasPath || topHasPath || bottomHasPath;
+        if (!hasPath) {
+            strLength--;
+            visited[curIndex] = NO;
+        }
+    }
+    return hasPath;
 }
 
 
